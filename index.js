fcea1bd246c06d79b19d02e3a3d0f1bccc341f69
Nate Tan <nktan93@gmail.com>
2019-08-27 21:42:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ class Driver {
     return parseInt(date, 10) - this.startDate.getFullYear()
   }
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+
+  blocksTravelled() {
+    Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal)) + Math.abs(parseInt(this.endingLocation.vertical) - parseInt(this.beginningLocation.vertical))
+  }
+}
