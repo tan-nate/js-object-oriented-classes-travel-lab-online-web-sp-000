98d1dd83e001e775984319c7c73c5f3f08f76a24
Nate Tan <nktan93@gmail.com>
2019-08-27 21:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ class Driver {
   }
 }
 
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
 class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
-
-  let eastWest = [
-    '1st Avenue',
-    '2nd Avenue',
-    '3rd Avenue',
-    'Lexington Avenue',
-    'Park',
-    'Madison Avenue',
-    '5th Avenue'
-  ];
 
   blocksTravelled() {
     return Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal)) + Math.abs(parseInt(this.endingLocation.vertical) - parseInt(this.beginningLocation.vertical))
